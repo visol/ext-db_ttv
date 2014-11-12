5ee46813781e7d3815e88ba016c0029dff289c42
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2014-11-12 10:15:43 +0100

    Make compatible with TYPO3 6.2, raise version to 5.0.0

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,6 @@
 <?php
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
-t3lib_div::loadTCA('tt_content');
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
 //Rename backend columns to avoid naming annoyances in page-module with "oldStyleColumns"
 $TCA['tt_content']['columns']['colPos']['config']['items']['0']['0'] = 'TV-SP-0';
@@ -21,13 +20,8 @@ $TCA['tt_content']['columns']['colPos']['config']['items']['6']['0'] = 'TV-SP-6'
 $TCA['tt_content']['columns']['colPos']['config']['items']['6']['1'] = 6;
 $TCA['tt_content']['columns']['colPos']['config']['items']['7']['0'] = 'TV-SP-7';
 $TCA['tt_content']['columns']['colPos']['config']['items']['7']['1'] = 7;
-//Show "media" on pages of type shortcut
-t3lib_div::loadTCA('pages');
-// show "files" on pages of type 'shortcut'
-// not needed for TYPO3 4.2. Uncomment if you are using older TYPO3 versions.
-// $TCA['pages']['types']['4']['showitem'] = 'hidden;;;;1-1-1, doktype, title;;3;;2-2-2, subtitle, nav_hide, media;;;;4-4-4, shortcut;;;;3-3-3, shortcut_mode, TSconfig;;6;nowrap;5-5-5, storage_pid;;7, l18n_cfg, tx_templavoila_ds;;;;1-1-1, tx_templavoila_to, tx_templavoila_next_ds, tx_templavoila_next_to, tx_templavoila_flex;;;;1-1-1';
 
-t3lib_extMgm::addPlugin(array('LLL:EXT:db_ttv/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPlugin(array('LLL:EXT:db_ttv/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
 
-t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Templates for TemplaVoila");
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,"pi1/static/","Templates for TemplaVoila");
 ?>
--- pi1/class.tx_dbttv_pi1.php
+++ pi1/class.tx_dbttv_pi1.php
@@ -1,85 +1,80 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*  
-*  (c) 2005-2008 Dieter Bunkerd (dieter.bunkerd@t3net.in.th)
-*  All rights reserved
-*  
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2005-2008 Dieter Bunkerd (dieter.bunkerd@t3net.in.th)
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the textfile GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
-	/**
-	 * Plugin 'tx_dbttv_pi1' for the 'db_ttv' extension.
-	 *
-	 * @author		Andreas Stauder, Dieter Bunkerd
-	 * @version		4.0.4
-	 */
+/**
+ * Plugin 'tx_dbttv_pi1' for the 'db_ttv' extension.
+ *
+ * @author        Andreas Stauder, Dieter Bunkerd
+ * @version        4.0.4
+ */
+class tx_dbttv_pi1 extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
+	var $prefixId = 'tx_dbttv_pi1'; // Same as class name
+	var $scriptRelPath = 'pi1/class.tx_dbttv_pi1.php'; // Path to this script relative to the extension dir.
+	var $extKey = 'db_ttv'; // The extension key.
+	var $pi_checkCHash = TRUE;
 
-require_once(PATH_tslib.'class.tslib_pibase.php');
-
-class tx_dbttv_pi1 extends tslib_pibase {
-	var $prefixId      = 'tx_dbttv_pi1';		// Same as class name
-	var $scriptRelPath = 'pi1/class.tx_dbttv_pi1.php';	// Path to this script relative to the extension dir.
-	var $extKey        = 'db_ttv';	// The extension key.
-	var $pi_checkCHash = true;
-
-	function main($content,$conf)	{
+	function main($content, $conf) {
 		$this->dbttvstyle = $conf;
-		$err_flag = 0; 
+		$err_flag = 0;
 		$currLevel = 0;
-		$returnCode = 0; 
+		$returnCode = 0;
 		$currFieldValue = '';
 		$currParentID = '';
 		$loopEnd = 0;
-		
-		if(isset($this->dbttvstyle["uid"])) $search_uid = $this->dbttvstyle["uid"]; else $search_uid = $GLOBALS['TSFE']->id;
-		if(isset($this->dbttvstyle["field"])) $search_dbttvfield = $this->dbttvstyle["field"]; else {$err_flag = 1; $defReturnValue="tx_dbttv_no_field_specified";}
 
-		$datastructure_uid = 0; 
+		if (isset($this->dbttvstyle["uid"])) $search_uid = $this->dbttvstyle["uid"]; else $search_uid = $GLOBALS['TSFE']->id;
+		if (isset($this->dbttvstyle["field"])) $search_dbttvfield = $this->dbttvstyle["field"]; else {
+			$err_flag = 1;
+			$defReturnValue = "tx_dbttv_no_field_specified";
+		}
+
+		$datastructure_uid = 0;
 		$startpage = 1;
-		
-		while($datastructure_uid == 0 && $search_uid != 0) {
-			$findit = mysql_query("SELECT pid, tx_templavoila_ds, tx_templavoila_next_ds FROM pages WHERE uid = $search_uid");
-		  if(mysql_num_rows($findit)>=1) {
-		    !$startpage ? $datastructure_uid = mysql_result($findit, 0, 2) : $startpage = 0;
-				if($datastructure_uid == 0) $datastructure_uid = mysql_result($findit, 0, 1);
-				if($datastructure_uid != 0) {
-					$datastructure = mysql_query("SELECT dataprot FROM tx_templavoila_datastructure WHERE uid = $datastructure_uid");
-		      if(mysql_num_rows($datastructure) >= 1) {
-						$dataprod = mysql_result($datastructure, 0, 0);
-						$xmlstructure = t3lib_div::xml2array($dataprod);
+
+		while ($datastructure_uid == 0 && $search_uid != 0) {
+			$findit = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('pid, tx_templavoila_ds, tx_templavoila_next_ds', 'pages', 'uid = ' . $search_uid);
+			if (count($findit)) {
+				!$startpage ? $datastructure_uid = $findit['tx_templavoila_next_ds'] : $startpage = 0;
+				if ($datastructure_uid == 0) $datastructure_uid = $findit['tx_templavoila_ds'];
+				if ($datastructure_uid != 0) {
+					$datastructure = $GLOBALS['TYPO3_DB']->exec_SELECTgetSingleRow('dataprot', 'tx_templavoila_datastructure', 'uid = ' . $datastructure_uid);
+					if (count($datastructure)) {
+						$xmlstructure = \TYPO3\CMS\Core\Utility\GeneralUtility::xml2array($datastructure['dataprot']);
 						return $xmlstructure['ROOT']['dbttvdata'][$search_dbttvfield];
 					}
 				}
-				$search_uid = mysql_result($findit, 0, 0);
-			}
-			else {
+				$search_uid = $findit['pid'];
+			} else {
 				$search_uid = 0;
-		  }
+			}
 		}
 		return '';
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/db_ttv/pi1/class.tx_dbttv_pi1.php']) {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/db_ttv/pi1/class.tx_dbttv_pi1.php']);
-}
 
 ?>
